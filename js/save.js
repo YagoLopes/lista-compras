94ebe0331a61dbdb7657abed55f7c4af218374e8
Yago Lopes <yagolopeslazaro@gmail.com>
2022-03-08 19:15:30 -0300

    feat(js): add script .js

---
+++ js/save.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form-lista');
+form.addEventListener('submit', saveFormInfo);
+
+function saveFormInfo(event) {
+  const InputNameProduto = document.getElementById('input-name-produto');
+  console.log(InputNameProduto.target);
+  const InputPriceProduto = document.getElementById('input-price-produto');
+  event.preventDefault();
+  alert('Deu certo!!');
+}
