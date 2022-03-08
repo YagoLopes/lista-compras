0f96ee8ed4b20bc7ba1be6ed421821b2b1d5af5a
Yago Lopes <yagolopeslazaro@gmail.com>
2022-03-08 19:42:31 -0300

    feat(js): add js matheus

--- js/save.js
+++ js/save.js
@@ -24,3 +24,11 @@ function saveFormInfo2(event) {
   event.preventDefault();
   alert('Deu certo!!');
 }
+
+function saveFormInfo2(event) {
+  const InputNameProduto = document.getElementById('input-name-produto');
+  console.log(InputNameProduto.target);
+  const InputPriceProduto = document.getElementById('input-price-produto');
+  event.preventDefault();
+  alert('Deu certo!!');
+}
